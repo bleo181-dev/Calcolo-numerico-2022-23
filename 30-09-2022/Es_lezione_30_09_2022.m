668ccd4b31a0b284e06b11ffdaec02ce60758e77
bleo181-dev <leonardo.bombonati@edu.unife.it>
2022-10-03 10:43:01 +0200

    Laboratorio del 30 settembre 2022

---
+++ 30-09-2022/Es_lezione_30_09_2022.m
@@ -0,0 +1,13 @@
+% BOMBONATI LEONARDO
+% ESERCIZI ASSEGNATI A LEZIONE - 30/09/2022
+
+clear all % cancella tutte le variabili nel workspace
+close all % chiude tutte le finestre aperte
+clc % cancella tutti i comandi sulla shell
+
+coeff = [1:4];
+alpha = 4;
+
+ruffini(coeff, alpha) % mia funzione
+polyval(coeff, alpha) % funzione matlab per ruffini
+
---
+++ 30-09-2022/ruffini.m
@@ -0,0 +1,14 @@
+function [palpha,q] = ruffini(coeff,alpha)
+%ruffini - Metodo di Ruffini-Horner per la val di un pol in un pto.
+
+n = numel(coeff) - 1; % grado del polinomio p(x)
+q = zeros(size(coeff)); % pre-allocazione del vettore quoziente
+
+q(1) = coeff(1)
+    for i = 2:(n+1)
+        q(i) = q(i-1)*alpha + coeff(i);
+    end
+palpha = q(end); 
+q = q(1:(end-1)); % alternativa: q(end) = []
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+end
