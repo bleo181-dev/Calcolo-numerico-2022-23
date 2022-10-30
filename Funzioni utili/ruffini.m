880a72b719479572e1b65617603c076b36976a75
bleo181-dev <leonardo.bombonati@edu.unife.it>
2022-10-30 09:09:25 +0100

    Gauss 2 e migliorie alle funzioni principali

--- Funzioni utili/ruffini.m
+++ Funzioni utili/ruffini.m
@@ -1,12 +1,35 @@
-function [palpha,q] = ruffini(coeff,alpha)
-%ruffini - Metodo di Ruffini-Horner per la val di un pol in un pto.
-    n = numel(coeff) - 1; % grado del polinomio p(x)
-    q = zeros(size(coeff)); % pre-allocazione del vettore quoziente
-    
-    q(1) = coeff(1);
-        for i = 2:(n+1)
-            q(i) = q(i-1)*alpha + coeff(i);
-        end
-    palpha = q(end); 
-    q = q(1:(end-1)); % alternativa: q(end) = []                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+function [r , q ] = ruffiniHorner (p , a )
+% RUFFINIHORNER - Schema di Ruffini - Horner
+% Calcola il valore di un polinomio p ( x ) nel punto x = a e i coefficienti
+% del polinomio quoziente q ( x ) = p ( x ) / ( x - a )
+% SYNOPSIS
+%   [r , q ] = ruffiniHorner (p , a )
+% INPUT
+%   p ( double array ) - Vettore dei coefficienti del polinomio , ordinati
+%                        da quello di grado piu ’ alto a quello di grado zero
+%   a ( double )       - Punto ( numero reale ) in cui calcolare il polinomio
+% OUTPUT
+%   r ( double )       - Valore del polinomio nel punto x = a
+%   q ( double array ) - Vettore dei coefficienti del polinomio quoziente
+%                        q(x) = p(x) / (x - a)
+%
+r = [];
+if ( isempty ( p ) )
+    q = [];
+    warning ( " Il vettore p dei coefficienti e' vuoto ") ;
+    return
+elseif ( isempty ( a ) )
+    q = p;
+    warning ( " Il punto ''a'' in cui valutare il polinomio e' vuoto ") ;
+    return
+else
+    n = numel ( p ) - 1; % grado del polinomio
+    q = zeros (n , 1) ;
+    q (1) = p (1) ;
+    for i = 2 : n +1
+        q ( i ) = q (i -1) * a + p ( i ) ;
+    end
+    r = q ( n +1) ;
+    q = q (1: n ) ;
 end
+end % fine della function ruffiniHorner
--- Funzioni utili/solupper.m
+++ Funzioni utili/solupper.m
@@ -1,11 +1,25 @@
-function [x] = solupper(R,b)
-% SOLUPPER - Soluzione di sistema triang. sup. (per righe)
-    n = length(b);        
-    x = b;                  % si puo  evitare con:
-    x(n) = x(n)/R(n,n);     % x(n) = b(n)/R(n,n);
-    for i = n-1 : -1 : 1    % e poi mettendo dentro il ciclo:
-        % SDOT - BLAS1
-        x(i) = x(i) - R(i, i+1:n)*x(i+1:n); % x(i) = b(i) - R(i,...
-        x(i) = x(i)/R(i,i);
+function [ x ] = solupper (R , b )
+% SOLUPPER - Soluzione di sistema triangolare superiore non singolare ( per righe )
+% SYNOPSIS
+%   [ x ] = solupper (R , b )
+% INPUT
+%   R ( double array ) - Matrice triangolare superiore non singolare
+%   b ( double array ) - Vettore dei termini noti
+% OUTPUT
+%   x ( double array ) - Vettore soluzione del sistema
+%
+[m , n ] = size ( R ) ;
+if ( m ~= n )
+    error ("Matrice di coefficienti non quadrata") ;
+elseif ( any ( find ( abs ( diag ( R ) ) < eps * norm (R , inf ) ) ) )
+    error ("Almeno un elemento diagonale di R e’ numericamente troppo vicino a zero") ;
+else
+    x = b;
+    x ( n ) = x ( n ) / R (n , n ) ;
+    for i = n -1 : -1 : 1
+    % SDOT - BLAS1
+    x ( i ) = x ( i ) - R (i , i +1: n ) * x ( i +1: n ) ;
+    x ( i ) = x ( i ) / R (i , i ) ;
     end
 end
+end % fine della function solupper
